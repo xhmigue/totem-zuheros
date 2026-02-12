d83cfde55ce79bb164d0c3b1b5ac71ace78aae4c
Your Name <you@example.com>
2026-02-12 19:25:05 +0100

    Subir servidor video 1

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ rm -rf /Users/xhmigue/apps/react/totem-zuheros-build
 cp -r dist/* /Users/xhmigue/apps/react/totem-zuheros-build
 cp .gitignore /Users/xhmigue/apps/react/totem-zuheros-build
 cp -r assets/images /Users/xhmigue/apps/react/totem-zuheros-build
+cp -r server/* /Users/xhmigue/apps/react/totem-zuheros-build
 
 cd /Users/xhmigue/apps/react/totem-zuheros-build
 git add .
